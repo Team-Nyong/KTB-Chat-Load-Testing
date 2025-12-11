eee47c8d20e9ad2e69861602f1c1be42320aafae
hwanvely <990706leo@gmail.com>
2025-12-11 17:19:43 +0900

    fix: UserService 프로필 이미지 URL로 변경

--- apps/backend/src/main/java/com/ktb/chatapp/service/FileService.java
+++ apps/backend/src/main/java/com/ktb/chatapp/service/FileService.java
@@ -2,9 +2,11 @@ package com.ktb.chatapp.service;
 
 import com.ktb.chatapp.dto.S3FileResponse;
 import com.ktb.chatapp.dto.S3UploadRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
     FileUploadResult saveS3Metadata(S3UploadRequest request, String uploaderId);
+
     S3FileResponse getFile(String fileId);
 }
